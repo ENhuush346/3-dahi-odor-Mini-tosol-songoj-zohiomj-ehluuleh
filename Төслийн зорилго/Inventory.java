5adc13e63e772fbfd00c50628bd74f7db8a1fcdf
Энхбаяр <sakask12331@gmail.com>
2025-06-04 13:33:13 +0800

    Update Inventory.java

--- Төслийн зорилго/Inventory.java
+++ Төслийн зорилго/Inventory.java
@@ -1,10 +1,18 @@
 import java.util.ArrayList;
 
 public class Inventory {
-    private ArrayList<Product> products = new ArrayList<>();
+    private ArrayList<Product> products;
 
-    public void addProduct(Product p) { products.add(p); }
+    public Inventory() {
+        products = new ArrayList<>();
+    }
+
+    public void addProduct(Product p) {
+        products.add(p);
+    }
+
     public void showInventory() {
+        System.out.println("=== 📋 Агуулах дахь барааны жагсаалт ===");
         for (Product p : products) {
             p.printInfo();
         }
